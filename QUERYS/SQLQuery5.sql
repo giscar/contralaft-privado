a3552f7baa96a0a20c04edfe4c03c49b82e21723
carlos leon <carlosleon@MacBook-Pro-de-carlos.local>
2020-06-28 01:14:43 -0500

    adecuaciones para mac

--- QUERYS/SQLQuery5.sql
+++
@@ -1,8 +0,0 @@
-W+++++++++++++++++++++++Script for SelectTopNRows command from SSMS  ******/GBNNNNNNNNNNNNNNNNNNNNNNNNN
-SELECT TOP (1000) [N_ID_ACCION]
-      ,[C_NUM_CODIGO]
-      ,[C_DES_NOMBRE]
-      ,[C_DES_DESCRIPCION]
-      ,[N_FL_ACTIVO]
-      ,[C_COD_ESTADO]
-  FROM [privado].[dbo].[ACCION]
