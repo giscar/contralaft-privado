1226712ce7414357b42d21852cd164b4d3f264d4
giscar <careli_2710@hotmail.com>
2021-05-03 18:04:38 -0500

    modificaciones a las funcionalidades de meta

--- SBS.UIF.CONTRALAFT.Entity/Core/Meta.cs
+++ SBS.UIF.CONTRALAFT.Entity/Core/Meta.cs
@@ -6,19 +6,21 @@ namespace SBS.UIF.CONTRALAFT.Entity.Core
 
     public class Meta
     {
-     public int Id { get; set; }
-     public int IdIndicador { get; set; }
-     public int IdEntidad { get; set; }
-     public int NumeroBase { get; set; }
-     public string MedioVerificacion { get; set; }
-     public string Descripcion { get; set; }
-     public string CodigoDocumento { get; set; }
-     public int FlActivo { get; set; }
-     public int Estado { get; set; }
-     public string UsuRegistro { get; set; }
-     public DateTime FecRegistro { get; set; }
-     public string UsuModificacion { get; set; }
-     public DateTime FecModificacion { get; set; }
+        public int Id { get; set; }
+        public int IdIndicador { get; set; }
+        public int IdEntidad { get; set; }
+        public int NumeroBase { get; set; }
+        public string MedioVerificacion { get; set; }
+        public string Descripcion { get; set; }
+        public string CodigoDocumento { get; set; }
+        public string CodExtension { get; set; }
+        public int FlActivo { get; set; }
+        public int Estado { get; set; }
+        public string UsuRegistro { get; set; }
+        public DateTime FecRegistro { get; set; }
+        public string UsuModificacion { get; set; }
+        public DateTime FecModificacion { get; set; }
+        public string ImageFile { get; set; }
     }
 }
 #pragma warning restore 1591
--- SBS.UIF.CONTRALAFT.Web/js/pages/listadoIndicadoresEntidad.js
+++ SBS.UIF.CONTRALAFT.Web/js/pages/listadoIndicadoresEntidad.js
@@ -20,3 +20,44 @@
         });
     });
 })(jQuery);
+
+function validaBorradorMetaClient() {
+
+    if ($('.txtMedioVerificacion').val().trim() === '') {
+        $('.txtMedioVerificacion').addClass('form-control-danger');
+        $('.txtMedioVerificacionLabel').addClass('text-danger').removeClass('text-muted');
+        return false;
+    } else {
+        $('.txtMedioVerificacion').removeClass('form-control-danger');
+        $('.txtMedioVerificacionLabel').removeClass('text-danger').addClass('text-muted');
+    }
+
+    if ($('.txtNumero').val().trim() === '') {
+        $('.txtNumero').addClass('form-control-danger');
+        $('.txtNumeroLabel').addClass('text-danger').removeClass('text-muted');
+        return false;
+    } else {
+        $('.txtNumero').removeClass('form-control-danger');
+        $('.txtNumeroLabel').removeClass('text-danger').addClass('text-muted');
+    }
+
+    if ($('.txtDescripcion').val().trim() === '') {
+        $('.txtDescripcion').addClass('form-control-danger');
+        $('.txtDescripcionLabel').addClass('text-danger').removeClass('text-muted');
+        return false;
+    } else {
+        $('.txtDescripcion').removeClass('form-control-danger');
+        $('.txtDescripcionLabel').removeClass('text-danger').addClass('text-muted');
+    }
+    
+    return true;
+}
+
+$(".soloNumeros").bind('keypress', function (event) {
+    var regex = new RegExp("^[0-9]+$");
+    var key = String.fromCharCode(!event.charCode ? event.which : event.charCode);
+    if (!regex.test(key)) {
+        event.preventDefault();
+        return false;
+    }
+});
